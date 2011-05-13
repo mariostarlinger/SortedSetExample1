02eec52a9e0cba85408ef249f5cbec637eca906a
Mario <mario_starlinger@gmx.at>
2011-05-13 15:45:32 +0200

    <Tag2> Barbara: Default-Konstruktor kommentiert

--- src/at/jku/ce/DomainObject.java
+++ src/at/jku/ce/DomainObject.java
@@ -4,6 +4,14 @@ public class DomainObject {
 
 	public String id;
 
+
+	/**
+ 	* Default constructor
+ 	*/
+	public DomainObject(){
+		super();
+	}
+
 	public DomainObject(String id) {
 		super();
 		this.id = id;
